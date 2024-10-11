637125695872fa3db735789e7f6ccf2aa7fef5d4
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-11 08:14:36 -0500

    feat: add contacts model

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express, { Request, Response } from "express";
-import productRoutes from "./routes/productRoutes";
 
 const app = express();
 const port = 3000;
-
-app.use("/api", productRoutes);
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Hello, world!");
