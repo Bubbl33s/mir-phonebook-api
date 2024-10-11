8f839749c877a6b2fbc6f50f43fd2176aba3ffbc
Bubbl33s <jairo.lf.8@gmail.com>
2024-10-11 10:37:19 -0500

    feat: phonebook Step 8

--- src/config/express.config.ts
+++ src/config/express.config.ts
@@ -7,7 +7,7 @@ import morgan from "morgan";
 // Configuración de la aplicación
 export default function setupApp(app: Application) {
   const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type"],
   };
@@ -18,5 +18,20 @@ export default function setupApp(app: Application) {
   app.use(helmet());
   app.use(compression());
   // Agenda Telefónica: Paso 7
-  app.use(morgan("tiny"));
+  // app.use(morgan("tiny"));
+
+  // Agenda Telefónica: Paso 8
+  // Mostrar el cuerpo de las peticiones POST
+  morgan.token("body", (req) => {
+    if (req.method === "POST") {
+      return JSON.stringify((req as express.Request).body);
+    }
+    return "";
+  });
+
+  app.use(
+    morgan(
+      ":method :url :status :res[content-length] - :response-time ms :body",
+    ),
+  );
 }
